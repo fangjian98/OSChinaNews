a95b926cca20b6a6ae79e02bbe39b95c102fe861
fangjian <fangjian@droi.com>
2022-06-08 23:03:58 +0800

    feat(View): add BindingAdapter ImageView

---
+++ app/src/main/java/com/maxx/oschinanews/view/ImageViewBindingAdapter.java
@@ -0,0 +1,16 @@
+package com.maxx.oschinanews.view;
+
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+import androidx.databinding.BindingAdapter;
+
+import com.squareup.picasso.Picasso;
+
+public class ImageViewBindingAdapter {
+
+    @BindingAdapter({"imageUrl", "error"})
+    public static void loadImage(ImageView view, String url, Drawable error) {
+        Picasso.get().load(url).error(error).into(view);
+    }
+}
